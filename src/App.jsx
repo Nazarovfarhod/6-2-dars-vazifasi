abb3933bb276f6e1e1f01206f73f46bbb9b6f1ea
farxodnazarov <farhodnazarov002gmail.com>
2024-06-14 16:23:05 +0500

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./index.css";
 
 const App = () => {
   const products = [
@@ -164,6 +165,64 @@ const App = () => {
           reviewerName: "Hazel Gardner",
           reviewerEmail: "hazel.gardner@x.dummyjson.com",
         },
+        {
+          id: 5,
+          title: "Apple",
+          description:
+            "Fresh and crisp apples, perfect for snacking or incorporating into various recipes.",
+          category: "groceries",
+          price: 1.99,
+          discountPercentage: 1.97,
+          rating: 2.96,
+          stock: 9,
+          tags: ["fruits"],
+          sku: "QTROUV79",
+          weight: 8,
+          dimensions: {
+            width: 8.29,
+            height: 5.58,
+            depth: 12.41,
+          },
+          warrantyInformation: "2 year warranty",
+          shippingInformation: "Ships in 2 weeks",
+          availabilityStatus: "In Stock",
+          reviews: [
+            {
+              rating: 4,
+              comment: "Great product!",
+              date: "2024-05-23T08:56:21.620Z",
+              reviewerName: "Logan Lee",
+              reviewerEmail: "logan.lee@x.dummyjson.com",
+            },
+            {
+              rating: 4,
+              comment: "Great product!",
+              date: "2024-05-23T08:56:21.620Z",
+              reviewerName: "Elena Long",
+              reviewerEmail: "elena.long@x.dummyjson.com",
+            },
+            {
+              rating: 1,
+              comment: "Not as described!",
+              date: "2024-05-23T08:56:21.620Z",
+              reviewerName: "Grayson Coleman",
+              reviewerEmail: "grayson.coleman@x.dummyjson.com",
+            },
+          ],
+          returnPolicy: "60 days return policy",
+          minimumOrderQuantity: 44,
+          meta: {
+            createdAt: "2024-05-23T08:56:21.620Z",
+            updatedAt: "2024-05-23T08:56:21.620Z",
+            barcode: "2517819903837",
+            qrCode: "https://dummyjson.com/public/qr-code.png",
+          },
+          images: [
+            "https://cdn.dummyjson.com/products/images/groceries/Apple/1.png",
+          ],
+          thumbnail:
+            "https://cdn.dummyjson.com/products/images/groceries/Apple/thumbnail.png",
+        },
       ],
       returnPolicy: "60 days return policy",
       minimumOrderQuantity: 25,
@@ -238,33 +297,157 @@ const App = () => {
       thumbnail:
         "https://cdn.dummyjson.com/products/images/beauty/Red%20Lipstick/thumbnail.png",
     },
+    {
+      id: 5,
+      title: "Asus Zenbook Pro Dual Screen Laptop",
+      description:
+        "The Asus Zenbook Pro Dual Screen Laptop is a high-performance device with dual screens, providing productivity and versatility for creative professionals.",
+      category: "laptops",
+      price: 1799.99,
+      discountPercentage: 9.21,
+      rating: 3.14,
+      stock: 38,
+      tags: ["laptops"],
+      brand: "Asus",
+      sku: "4A9KFMBG",
+      weight: 8,
+      dimensions: {
+        width: 19.67,
+        height: 11.05,
+        depth: 14.32,
+      },
+      warrantyInformation: "No warranty",
+      shippingInformation: "Ships in 3-5 business days",
+      availabilityStatus: "In Stock",
+      reviews: [
+        {
+          rating: 5,
+          comment: "Very satisfied!",
+          date: "2024-05-23T08:56:21.622Z",
+          reviewerName: "Jack Ward",
+          reviewerEmail: "jack.ward@x.dummyjson.com",
+        },
+        {
+          rating: 1,
+          comment: "Very disappointed!",
+          date: "2024-05-23T08:56:21.622Z",
+          reviewerName: "Ava Harris",
+          reviewerEmail: "ava.harris@x.dummyjson.com",
+        },
+        {
+          rating: 5,
+          comment: "Great value for money!",
+          date: "2024-05-23T08:56:21.622Z",
+          reviewerName: "Brayden Fleming",
+          reviewerEmail: "brayden.fleming@x.dummyjson.com",
+        },
+      ],
+      returnPolicy: "No return policy",
+      minimumOrderQuantity: 1,
+      meta: {
+        createdAt: "2024-05-23T08:56:21.622Z",
+        updatedAt: "2024-05-23T08:56:21.622Z",
+        barcode: "2899455033799",
+        qrCode: "https://dummyjson.com/public/qr-code.png",
+      },
+      images: [
+        "https://cdn.dummyjson.com/products/images/laptops/Asus%20Zenbook%20Pro%20Dual%20Screen%20Laptop/1.png",
+        "https://cdn.dummyjson.com/products/images/laptops/Asus%20Zenbook%20Pro%20Dual%20Screen%20Laptop/2.png",
+        "https://cdn.dummyjson.com/products/images/laptops/Asus%20Zenbook%20Pro%20Dual%20Screen%20Laptop/3.png",
+      ],
+      thumbnail:
+        "https://cdn.dummyjson.com/products/images/laptops/Asus%20Zenbook%20Pro%20Dual%20Screen%20Laptop/thumbnail.png",
+    },
+    {
+      id: 6,
+      title: "Apple Watch Series 4 Gold",
+      description:
+        "The Apple Watch Series 4 in Gold is a stylish and advanced smartwatch with features like heart rate monitoring, fitness tracking, and a beautiful Retina display.",
+      category: "mobile-accessories",
+      price: 349.99,
+      discountPercentage: 5.14,
+      rating: 4.42,
+      stock: 68,
+      tags: ["electronics", "smartwatches"],
+      brand: "Apple",
+      sku: "ZNXPOEPT",
+      weight: 6,
+      dimensions: {
+        width: 8.96,
+        height: 28.26,
+        depth: 26.46,
+      },
+      warrantyInformation: "6 months warranty",
+      shippingInformation: "Ships in 1-2 business days",
+      availabilityStatus: "In Stock",
+      reviews: [
+        {
+          rating: 3,
+          comment: "Would not buy again!",
+          date: "2024-05-23T08:56:21.624Z",
+          reviewerName: "Ellie Stewart",
+          reviewerEmail: "ellie.stewart@x.dummyjson.com",
+        },
+        {
+          rating: 1,
+          comment: "Poor quality!",
+          date: "2024-05-23T08:56:21.624Z",
+          reviewerName: "Grayson Coleman",
+          reviewerEmail: "grayson.coleman@x.dummyjson.com",
+        },
+        {
+          rating: 4,
+          comment: "Fast shipping!",
+          date: "2024-05-23T08:56:21.624Z",
+          reviewerName: "Alice Smith",
+          reviewerEmail: "alice.smith@x.dummyjson.com",
+        },
+      ],
+      returnPolicy: "60 days return policy",
+      minimumOrderQuantity: 2,
+      meta: {
+        createdAt: "2024-05-23T08:56:21.624Z",
+        updatedAt: "2024-05-23T08:56:21.624Z",
+        barcode: "4492482209885",
+        qrCode: "https://dummyjson.com/public/qr-code.png",
+      },
+      images: [
+        "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Watch%20Series%204%20Gold/1.png",
+        "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Watch%20Series%204%20Gold/2.png",
+        "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Watch%20Series%204%20Gold/3.png",
+      ],
+      thumbnail:
+        "https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Watch%20Series%204%20Gold/thumbnail.png",
+    },
   ];
 
   let [state, setState] = useState(products);
   console.log(state);
 
   return (
-    <div>
+    <div className="cards">
       {state.map((item) => {
         return (
-          <div>
+          <div className="card">
             <img src={item.thumbnail} alt="" />
-            <h3>{item.title}</h3>
-            <p>
-              {" "}
+            <h3 className="title">{item.title}</h3>
+            <p className="old-price">
               <b>Old Price:</b> {item.price}
               <b>$</b>
             </p>
-            <div>
+            <div className="new-priceSale">
               <p>
-                <b>New Price:</b>
+                <b>New Price : </b>
                 {(
                   item.price -
                   (item.price * item.discountPercentage) / 100
                 ).toFixed(2)}
                 <b>$</b>
               </p>
-              <span> <b>Discount:</b> {item.discountPercentage}%</span>
+              <span>
+                <b>Sale : </b>
+                <span className="sale"> {item.discountPercentage}%</span>
+              </span>
             </div>
           </div>
         );
